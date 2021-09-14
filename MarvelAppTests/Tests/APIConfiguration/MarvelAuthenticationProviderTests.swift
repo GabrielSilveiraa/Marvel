13459b3886d86f7eb46cab4cca2fbf1e2adc5260
GabrielSilveiraa <gabi.msilveira@gmail.com>
2021-09-14 20:31:48 +0100

    Done tests for API Authentication

---
+++ MarvelAppTests/Tests/APIConfiguration/MarvelAuthenticationProviderTests.swift
@@ -0,0 +1,21 @@
+//
+//  MarvelAuthenticationProviderTests.swift
+//  MarvelAppTests
+//
+//  Created by Gabriel Miranda Silveira on 14/09/21.
+//
+
+import XCTest
+@testable import MarvelApp
+
+class MarvelAuthenticationProviderTests: XCTestCase {
+    func testGetAuthentication() {
+        let expected = MarvelAuthenticationProvider.getAuthentication(publicKey: "publicFakeKey",
+                                                                      privateKey: "privateFakeKey",
+                                                                      timestamp: 123456) as? [String: String]
+
+        XCTAssertEqual(expected, ["apiKey": "publicFakeKey",
+                                  "hash": "631878bdbb79b05ebbfeb07f1467c776",
+                                  "ts": "123456"])
+    }
+}
