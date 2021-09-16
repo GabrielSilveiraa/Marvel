93b00129d7e75cd931f68db30576fb2de93fda21
GabrielSilveiraa <gabi.msilveira@gmail.com>
2021-09-16 23:38:24 +0100

    Added app appearance configuration

---
+++ MarvelApp/Extensions/UIColor+MarvelColors.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+MarvelColors.swift
+//  MarvelApp
+//
+//  Created by Gabriel Miranda Silveira on 16/09/21.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    static let redMarvel = UIColor(red: 231/255, green: 39/255, blue: 48/255, alpha: 0.85)
+    static let blackMarvel = UIColor.black.withAlphaComponent(0.85)
+}
